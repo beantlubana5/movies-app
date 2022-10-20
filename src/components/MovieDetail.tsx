128badd9fdc60d19ffbecd0cc0f7b1d2c0b92dce
suresh827 <suresh.namala@hotmail.com>
2022-10-19 21:33:31 -0400

    make images show

--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -4,6 +4,8 @@ import MovieDetails from "../models/MovieDetails";
 import { getMovieDetailsById } from "../services/MoviesService";
 import Movie from "./Movie";
 import "./MovieDetail.css";
+const image_url: string =
+  process.env.REACT_APP_BASE_IMAGE_URL || "https://image.tmdb.org/t/p/w500";
 
 const MovieDetail = () => {
   const id: string | undefined = useParams().id;
@@ -15,6 +17,11 @@ const MovieDetail = () => {
     });
   }, [id]);
 
+  const buildImageUrl = (image: string) => {
+    console.log(image_url + image);
+    return image_url + image;
+  };
+
   return (
     <div className="MovieDetails">
       {movieDetail ? (
@@ -22,9 +29,13 @@ const MovieDetail = () => {
           <h1>{movieDetail.title}</h1>
           <p>{movieDetail.budget}</p>
           <p>{movieDetail.overview}</p>
+          <img
+            src={buildImageUrl(movieDetail.poster_path)}
+            alt={movieDetail.title}
+          />
         </div>
       ) : (
-        <p>Loading</p>
+        <p>No Luck!!</p>
       )}
     </div>
   );
